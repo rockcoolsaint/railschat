b6947c9b858659a0ff3133e7c54ecf3cccf683b4
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-11-30 22:25:43 +0100

    Added Devise avatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'rooms/index'
   resources :rooms do
     resources :messages
   end
   root 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    get 'users', to: 'devise/sessions#new'
-  end
+  # devise_scope :user do
+  #   get 'users', to: 'devise/sessions#new'
+  # end
   # get 'user/:id', to: 'users#show', as: 'user'
   # Alternatively use the resources route as below. It achieves the same result
   resources :users, only: [:show]
